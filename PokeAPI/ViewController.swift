b98c08ac150517cc238eef83051d1ac3b16a4b0a
Etudiant <Etudiant@ld140-21-06268.local>
2016-11-24 14:33:07 -0500

    Départ

--- PokeAPI/ViewController.swift
+++ PokeAPI/ViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let UrlDebut = "https://pokeapi.co/api/v2"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
