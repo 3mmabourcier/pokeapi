27e80510fa1ff8b9deaf91dfde7f1e7894d2f6e8
Etudiant <Etudiant@ld140-21-06268.local>
2016-11-24 16:07:46 -0500

    Cherche les pokemons sur l'api et print

--- PokeAPI/ViewController.swift
+++ PokeAPI/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pokemonUrlList = Array<Dictionary<String, String>>()
+    var pokemonList = Array<Dictionary<String, Any>>()
 
-    let UrlDebut = "https://pokeapi.co/api/v2"
-    var UrlFin = ""
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let limit = 721
+        let UrlPokemon = "https://pokeapi.co/api/v2/pokemon/?limit=\(limit)"
+        let DepartUrl = URL(string: UrlPokemon)!
+        
+        
+        getPokemonListFromUrl(leUrl: DepartUrl)
+        
+        for pokemon in pokemonUrlList{
+            if let _url = pokemon["url"] as String? {
+                getPokemonFromUrl(leUrl: URL(string: _url)!)
+                print(pokemonList)
+            }
+            
+        }
+        
+        
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -24,6 +39,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getPokemonListFromUrl(leUrl : URL) {
+        
+        /// DispatchQueue.main.async ( execute: {
+        if let _données = NSData(contentsOf: leUrl) as? Data {
+            do {
+                let json = try JSONSerialization.jsonObject(with: _données, options: JSONSerialization.ReadingOptions()) as? Dictionary<String, Any>
+                
+                print("Conversion JSON réussie")
+                //print(json)
+                self.pokemonUrlList = json?["results"] as! Array<Dictionary<String, String>>
+                //return json!
+            } catch {
+                print("\n\n#Erreur: Problème de conversion json:\(error)\n\n")
+            } // do/try/catch
+        } else
+        {
+            print("\n\n#Erreur: impossible de lire les données via:\(leUrl.absoluteString)\n\n")
+        } // if let _données = NSData
+        
+        /// }) // DispatchQueue.main.async
+    }
+    
+    func getPokemonFromUrl(leUrl : URL) {
+        
+        /// DispatchQueue.main.async ( execute: {
+        if let _données = NSData(contentsOf: leUrl) as? Data {
+            do {
+                let json = try JSONSerialization.jsonObject(with: _données, options: JSONSerialization.ReadingOptions()) as? Dictionary<String, Any>
+                
+                print("Conversion JSON réussie")
+
+                
+                self.pokemonList.append( json as Dictionary<String, Any>!)
+                
+            } catch {
+                print("\n\n#Erreur: Problème de conversion json:\(error)\n\n")
+            } // do/try/catch
+        } else
+        {
+            print("\n\n#Erreur: impossible de lire les données via:\(leUrl.absoluteString)\n\n")
+        } // if let _données = NSData
+        
+        /// }) // DispatchQueue.main.async
+    }
 
 }
 
