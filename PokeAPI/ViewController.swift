a669d8a32474dca9a5dccacb4eeca1fd0c9a3826
Etudiant <Etudiant@ld140-21-06268.local>
2016-11-24 14:35:40 -0500

    Départ

--- PokeAPI/ViewController.swift
+++ PokeAPI/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let UrlDebut = "https://pokeapi.co/api/v2"
+    var UrlFin = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
