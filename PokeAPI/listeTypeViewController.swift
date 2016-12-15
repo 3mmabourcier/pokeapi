2188d3ce069789ba02d3ff499b7513456b7c7dca
Etudiant <Etudiant@ld140-21-06268.local>
2016-12-15 18:09:16 -0500

    Fin recherche par type 15/12/2016

--- PokeAPI/listeTypeViewController.swift
+++ PokeAPI/listeTypeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 protocol typeChoisiDelegate{
     func envoieDuType(info:String)
-}
+}//typeChoisiDelegate
 
 class listeTypeViewController: UIViewController, UICollectionViewDataSource {
 
